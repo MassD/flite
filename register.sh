9b5a21d0fb1492c3cd6686c3c0e9a8ef28afc5ed
Jack Chen <massd.me@gmail.com>
2013-06-26 19:22:39 +0100

    changed register.sh

--- register.sh
+++ register.sh
@@ -1,19 +1,3 @@
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap CSX -dep-mo 2013-9 -dep-dy 16 -ret-mo 2013-10 -ret-dy 30 -user "Yifei" -email "dr.yifei.zeng@gmail.com" -frequency 0::8::12::14::16::20::22
+OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PVG -dep-mo 2013-9 -dep-dy 20 -ret-mo 2013-10 -ret-dy 14 -user "Jenny" -email "jennyindcs@gmail.com" -frequency 0::8::12::14::16::20::22
 
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap CSX -dep-mo 2013-9 -dep-dy 16 -ret-mo 2013-11 -ret-dy 06 -user "Yifei" -email "dr.yifei.zeng@gmail.com" -frequency 0::8::12::14::16::20::22
-
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PEK -dep-mo 2013-9 -dep-dy 16 -ret-mo 2013-10 -ret-dy 30 -user "Yifei" -email "dr.yifei.zeng@gmail.com" -frequency 0::8::12::14::16::20::22
-
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PEK -dep-mo 2013-9 -dep-dy 16 -ret-mo 2013-11 -ret-dy 06 -user "Yifei" -email "dr.yifei.zeng@gmail.com" -frequency 0::8::12::14::16::20::22
-
-
-
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PEK -dep-mo 2013-9 -dep-dy 23 -ret-mo 2013-10 -ret-dy 15 -user "Jenny" -email "jennyindcs@gmail.com" -frequency 0::8::12::14::16::20::22
-
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PEK -dep-mo 2013-9 -dep-dy 23 -ret-mo 2013-10 -ret-dy 15 -user "Xinuo" -email "iamindcs@gmail.com" -frequency 0::8::12::14::16::20::22
-
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PVG -dep-mo 2013-9 -dep-dy 23 -ret-mo 2013-10 -ret-dy 15 -user "Jenny" -email "jennyindcs@gmail.com" -frequency 0::8::12::14::16::20::22
-
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PVG -dep-mo 2013-9 -dep-dy 23 -ret-mo 2013-10 -ret-dy 15 -user "Xinuo" -email "iamindcs@gmail.com" -frequency 0::8::12::14::16::20::22
-
-OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap HKG -arr-ap LHR -dep-mo 2013-9 -dep-dy 15 -ret-mo 2013-9 -ret-dy 22 -user "Nan" -email "sherwin1216@gmail.com" -frequency 0::8::12::14::16::20::22
+OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PVG -dep-mo 2013-9 -dep-dy 20 -ret-mo 2013-10 -ret-dy 14 -user "Xinuo" -email "iamindcs@gmail.com" -frequency 0::8::12::14::16::20::22
