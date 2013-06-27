5be4bf3dc4ce87b506c22ef2085100bfa09a4b2e
Jack Chen <massd.me@gmail.com>
2013-06-27 21:12:21 +0100

    added yifei to alert

--- register.sh
+++ register.sh
@@ -1,3 +1,5 @@
 OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PVG -dep-mo 2013-9 -dep-dy 20 -ret-mo 2013-10 -ret-dy 14 -user "Jenny" -email "jennyindcs@gmail.com" -frequency 0::8::12::14::16::20::22
 
 OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LON -arr-ap PVG -dep-mo 2013-9 -dep-dy 20 -ret-mo 2013-10 -ret-dy 14 -user "Xinuo" -email "iamindcs@gmail.com" -frequency 0::8::12::14::16::20::22
+
+OCAMLRUNPARAM="b" ./reg_journey_cmd.native -dep-ap LGW -arr-ap CSX -dep-mo 2013-9 -dep-dy 16 -ret-mo 2013-11 -ret-dy 02 -user "Yifei" -email "dr.yifei.zeng@gmail.com" -frequency 0::8::12::14::16::20::22
