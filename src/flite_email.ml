430a5bf8f010224940a437530e82b86f9fc641dc
massd <massd.me@gmail.com>
2013-07-02 10:23:57 +0100

    iss#5: added last_fsed to email subject

--- src/flite_email.ml
+++ src/flite_email.ml
@@ -37,7 +37,7 @@ let email a j pl =
   let html = format_pl j pl in
   match html with 
     | Some h ->
-      let subj = Printf.sprintf "Flite: %s to %s, %s-%s, back on %s-%s, last checked on %s" j.dep_ap j.arr_ap j.dep_mo j.dep_dy j.ret_mo j.ret_dy (Utils.string_of_utime j.last_fsed) in
+      let subj = Printf.sprintf "Flite-%s: %s to %s, %s-%s, back on %s-%s" (Utils.string_of_utime j.last_fsed) j.dep_ap j.arr_ap j.dep_mo j.dep_dy j.ret_mo j.ret_dy in
       send_lwt (a.user, a.email, subj, h)
     | None -> return_unit
   
