7381d08e02d58fa857b8ffd04f62ace4eefb6963
massd <massd.me@gmail.com>
2013-07-05 13:48:28 +0100

    iss#7: use quoted-printable for ✈

--- src/flite_email.ml
+++ src/flite_email.ml
@@ -37,7 +37,8 @@ let email a j pl =
   let html = format_pl j pl in
   match html with 
     | Some h ->
-      let subj = "✈ " ^ (Printf.sprintf "%s to %s, %s-%s, back on %s-%s, checked on %s" j.dep_ap j.arr_ap j.dep_mo j.dep_dy j.ret_mo j.ret_dy (Utils.string_of_utime j.last_fsed)) in
+      (* ✈ in subject http://www.emailmarketingtipps.de/2012/02/21/specials-characters-and-symbols-in-email-subject-lines-does-it-work/, http://www.webatic.com/run/convert/qp.php *)
+      let subj = Printf.sprintf "?UTF-8?Q?=E2=9C=88?= %s to %s, %s-%s, back on %s-%s, checked on %s" j.dep_ap j.arr_ap j.dep_mo j.dep_dy j.ret_mo j.ret_dy (Utils.string_of_utime j.last_fsed) in
       send_lwt (a.user, a.email, subj, h)
     | None -> return_unit
   
